7648cd34858415664112971c4041ff08f6908813
Luca Piantadosi <lu.piantadosi125@gmail.com>
2024-02-28 19:14:00 +0100

    jumbotrone addedd

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script>
 //codice della logica della nostra app
 import Title from "./components/title.vue"
+import Apphero from './components/hero.vue';
 
 export default {
   components: {
     Title,
+    Apphero,
   }
 }
 
@@ -15,6 +17,7 @@ export default {
 <template>
 <!-- //il layout della logica della nostra app -->
   <Title></Title>
+  <Apphero></Apphero>
 
 </template>
 
---
+++ src/components/hero.vue
@@ -0,0 +1,51 @@
+<script>
+    export default{
+        name: 'hero'
+    }
+</script>
+
+<template>
+
+<div class="container">
+        <h2>LET'S PUSH THIS USELESS BUTTON</h2>
+        <button>Premi per il nulla cosmico</button>
+        <p id="txt">
+            Fa sempre bene premere un pulsante inutile
+        </p>
+        
+    </div>
+
+</template>
+
+
+
+<style>
+.container{
+    width: 500px;
+    border-radius: 15px;
+    background-color: bisque;
+    
+    padding: 15px 40px;
+    margin: auto;
+    font-size: 20px;
+}
+h2{
+    font-style: italic;
+    color: brown;
+}
+#txt{
+    color: brown;
+}
+button{
+    border-radius: 25px;
+    background-color:brown;
+    border: none;
+    color: #ffffff;
+    padding: 12px 30px;
+    font-size: 15px;
+    font-weight: bold;
+    margin: 4px 2px;
+    cursor: pointer;
+    
+}
+</style>
