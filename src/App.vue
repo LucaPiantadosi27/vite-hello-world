2fb2b7272d014b1bb1e8a8b6906af9f8260e6bc8
Luca Piantadosi <lu.piantadosi125@gmail.com>
2024-02-28 16:53:28 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+//codice della logica della nostra app
+</script>
+
+
+
+<template>
+<!-- //il layout della logica della nostra app -->
+  
+  <h1>Ciao </h1>
+</template>
+
+
+
+<style>
+/* //lo stile della logica della nostra app */
+
+
+</style>
