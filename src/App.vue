2558c5162d4cc139854796ae80f5a04475334145
Luca Piantadosi <lu.piantadosi125@gmail.com>
2024-02-28 17:36:47 +0100

    title added

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <script>
 //codice della logica della nostra app
+import Title from "./components/title.vue"
+
+export default {
+  components: {
+    Title,
+  }
+}
+
 </script>
 
 
 
 <template>
 <!-- //il layout della logica della nostra app -->
-  
-  <h1>Ciao </h1>
+  <Title></Title>
+
 </template>
 
 
---
+++ src/components/title.vue
@@ -0,0 +1,15 @@
+<script>
+    export default{
+        name: 'title'
+    }
+</script>
+
+<template>
+
+<h1>"La mia prima app con Vite!"</h1>
+
+</template>
+
+<style>
+
+</style>
